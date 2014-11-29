95b95a5b94c4265d952f01f13ae33481406fb84f
Adam Gawelczyk <adam.gawelczyk@motorolasolutions.com>
2014-11-29 13:34:21 +0100

    * map with searching directions works :-)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var toastGroupTemplate = document.querySelector('#toastGroup');
-toastGroupTemplate.showToast = function() {
-  document.querySelector('#toast').show();
-}
+var gMap = document.querySelector('google-map');
+gMap.addEventListener('api-load', function(e) {
+  document.querySelector('google-map-directions').map = this.map;
+});
